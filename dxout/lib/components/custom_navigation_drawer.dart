0e02176fdffa631d87ea43ed7e94c7c4411b9ca1
arely_paulina <rojas.arelypaulina.11gm.2016@gmail.com>
2023-11-03 17:11:43 -0600

    Added "CustomNavigationsDrawer" component

---
+++ dxout/lib/components/custom_navigation_drawer.dart
@@ -0,0 +1,76 @@
+import 'package:dxout/constants.dart';
+import 'package:dxout/screens/TaskCreate/task_create_screen.dart';
+import 'package:flutter/material.dart';
+
+class CustomNavigationDrawer extends StatelessWidget {
+  const CustomNavigationDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Drawer(
+        child: SingleChildScrollView(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                buildHeader(context),
+                buildItemsMenu(context)
+              ]),
+        ),
+      );
+
+  Widget buildHeader(BuildContext context) => Container(
+        color: menuColor,
+        padding: EdgeInsets.only(
+            top: 24 + MediaQuery.of(context).padding.top, bottom: 24),
+        child: Column(children: [
+          // Parte de la imagen
+          Image.asset(
+            'assets/images/chanel.png',
+            width: 52,
+          ),
+        ]),
+      );
+
+  Widget buildItemsMenu(BuildContext context) => Container(
+      padding: const EdgeInsets.all(24),
+      child: Wrap(
+        runSpacing: 16,
+        children: [
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Pantalla Principal'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.supervised_user_circle_outlined),
+            title: const Text('Listas permitidas / Perfiles'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.timer_outlined),
+            title: const Text('Fijar temporizador de bloqueo'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.checklist_outlined),
+            title: const Text('Lista de pendientes'),
+            onTap: () {},
+          ),
+          const Divider(
+            color: Colors.black54,
+            height: 0.5,
+            thickness: 0.5,
+          ),
+          ListTile(
+            leading: const Icon(Icons.help_outline),
+            title: const Text('Acerca de'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings_outlined),
+            title: const Text('Ajustes'),
+            onTap: () => Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => TaskCreateScreen())),
+          ),
+        ],
+      ));
+}
--- dxout/lib/screens/TaskCreate/task_create_screen.dart
+++ dxout/lib/screens/TaskCreate/task_create_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dxout/components/custom_navigation_drawer.dart';
 import 'package:dxout/constants.dart';
 import 'package:dxout/screens/TaskCreate/components/taskcreate_body.dart';
 import 'package:flutter/material.dart';
@@ -6,10 +7,8 @@ class TaskCreateScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            // Here we take the value from the MyHomePage object that was created by
-            // the App.build method, and use it to set our appbar title.
-            title: const Text("Lista de pendientes")),
+        appBar: AppBar(title: const Text("Lista de pendientes")),
+        drawer: const CustomNavigationDrawer(),
         backgroundColor: menuColor,
         body: TaskCreateBody(
           key: key,
